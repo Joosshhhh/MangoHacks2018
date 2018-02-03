52bee31f39b8ad874b2f84905cf6e2f09e9fafd2
Joosshhhh <jmcle019@fiu.edu>
2018-02-03 16:41:01 -0500

    Adding Alexa response function

--- src/CarnivalCruiseSpeechlet.java
+++ src/CarnivalCruiseSpeechlet.java
@@ -1,6 +1,5 @@
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.slu.Intent;
-import com.amazon.speech.speechlet.*;
 
 public class CarnivalCruiseSpeechlet implements SpeechletV2 {
 
@@ -32,5 +31,37 @@ public class CarnivalCruiseSpeechlet implements SpeechletV2 {
     @Override
     public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> speechletRequestEnvelope) {
 
+    }
+
+    /**
+     * Creates and returns a {@code SpeechletResponse} with a welcome message.
+     *
+     * @return SpeechletResponse spoken and visual welcome message
+     */
+    private SpeechletResponse getWelcomeResponse() {
+        // Create the welcome message.
+        String speechText =
+                "Welcome to the Carnival. Please tell me your favorite color by "
+                        + "saying, my favorite color is red";
+        String repromptText =
+                "Please tell me your favorite color by saying, my favorite color is red";
+
+        return getSpeechletResponse(speechText, repromptText, true);
+    }
+
+    /**
+     * Returns a Speechlet response for a speech and reprompt text.
+     */
+    private SpeechletResponse newAskResponse(String stringOutput,
+                                             String repromptText) {
+        OutputSpeech outputSpeech, repromptOutputSpeech;
+        outputSpeech = new PlainTextOutputSpeech();
+        ((PlainTextOutputSpeech) outputSpeech).setText(stringOutput);
+
+        repromptOutputSpeech = new PlainTextOutputSpeech();
+        ((PlainTextOutputSpeech) repromptOutputSpeech).setText(repromptText);
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptOutputSpeech);
+        return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
     }
 }
